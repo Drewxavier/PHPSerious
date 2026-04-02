bc5bd5aea793ac655e2c5f91eaa61dbaa2d4d045
Andrew Maina <mainaandrewcom@gmail.com>
2026-04-02 11:14:37 +0300

    Updated PHP notes and codes

--- Teaching.php
+++ Teaching.php
@@ -11,3 +11,66 @@ function test(){
 test();
 $arr = array("Yes", "no");
 var_dump($arr);
+
+//example of named arguments
+function foo($a, $b, $c = 3,$d = 4){
+    return $a + $b + $c + $d;
+}
+var_dump(foo(a:1, b: 2, d: 40));//can't conbine unpacking with named arguments
+var_dump(foo(b :2, a : 1, d: 40));
+var_dump(foo(...[1, 2]));
+//anonymous function using callable
+$double1= function($a){
+    return $a * 2;
+};
+
+//using first-class callable syntax
+function double_function($a){
+    return $a * 2;
+};
+
+$double2 = double_function(...);
+//using arrow function
+$double3 = fn($a) => $a * 2;
+
+//using closure
+$double4 = Closure::fromCallable('double_function');
+echo "<br>";
+
+// use the closure as a callback here to
+//double the size of each element in our range
+echo "Examples of use of closure";
+echo "<br>";
+$new_numbers = array_map($double1, range(1, 5));
+print implode(' ', $new_numbers) . PHP_EOL;
+echo "<br>";
+$new_numbers = array_map($double2, range(1, 6));
+print implode(' ', $new_numbers) . PHP_EOL;
+echo "<br>";
+$new_numbers = array_map($double3, range(1, 7));
+print implode(' ', $new_numbers) . PHP_EOL;
+echo "<br>";
+$new_numbers = array_map($double4, range(1, 8));
+print implode(' ', $new_numbers) . PHP_EOL;
+echo "<br>";
+
+//examples of variadic variables
+function varid(...$a){
+    return array_sum($a);
+}
+$sum= [ 1, 2, 3, 4, 5];
+$sum2 = [13, 2, 6];
+$result =varid(...$sum);//reason it isn't like the second one is due to passing one value, the $sum rather than multiple
+echo "The sum of the array " . implode(" ",$sum) . " is " . $result;
+echo "<br>";
+
+//Example two of variadic variables
+function sum(...$number){
+    $acc = 0;
+    foreach($number as $n){
+        $acc += $n;
+    }
+    return $acc;
+     }
+echo sum(1,2,3,4);
+echo "<br>";
