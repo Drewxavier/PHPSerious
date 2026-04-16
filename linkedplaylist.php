9f156f72e26f090c5808a874bfb2eb6300084eec
Drewxavier <mainaandrewcom@gmail.com>
2026-04-16 12:09:33 +0300

    LInked list practice

---
+++ linkedplaylist.php
@@ -0,0 +1,70 @@
+<?php
+class ListNode {
+    public $data;
+    public $next;
+
+    public function __construct($data) {
+        $this->data = $data;
+        $this->next = NULL;
+    }
+}
+
+class LinkedList {
+    private $head = NULL;
+
+    // Insert at end
+    public function insert($data) {
+        $newNode = new ListNode($data);
+        if ($this->head === NULL) {
+            $this->head = $newNode;
+        } else {
+            $current = $this->head;
+            while ($current->next !== NULL) {
+                $current = $current->next;
+            }
+            $current->next = $newNode;
+        }
+    }
+
+    // Traverse and print
+    public function traverse() {
+        $current = $this->head;
+        while ($current !== NULL) {
+            echo $current->data . " ";
+            $current = $current->next;
+        }
+        echo "\n";
+    }
+    public function reverse() {
+        $prev = NULL;
+        $current = $this->head;
+        $next = NULL;
+        while ($current !== NULL) {
+            //save the next node
+            $next = $current->next;
+
+            //Reverse the link
+            $current->next = $prev;
+
+            //Move foward
+            $prev = $current;
+            $current = $next;
+        }
+
+        // Update head to the new front
+        $this->head = $prev;
+    }
+}
+
+// Usage
+$list = new LinkedList();
+$list->insert(1);
+$list->insert(2);
+$list->insert(3);
+
+echo "Linked list: \n";
+$list->traverse();
+echo "\n";
+$list->reverse();
+echo "Reversed Linked List: \n";
+$list->traverse();
